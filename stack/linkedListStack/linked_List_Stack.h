6a5af26372f4ded07eec04cb85e0f2875d9a9dc8
MoBMoCaffeine <bakrm1921@gmail.com>
2026-02-01 00:17:15 +0200

    claening File Structure

--- stack/linkedListStack/linked_List_Stack.h
+++ stack/linkedListStack/linked_List_Stack.h
@@ -20,57 +20,18 @@ class Stack {
     size_t _size;
 
 public:
-    Stack() : _top(nullptr), _size(0) {}
-    ~Stack() {
-        clear();
-    }
+    Stack();
+    ~Stack();
 
-    void push(const T& value) {
-        Node<T>* newNode = new Node<T>;
-        newNode->data = value;
-        newNode->next = _top;
-        _top = newNode;
-        ++_size;
-    }
+    void push(const T& value);
+    void pop();
+    const T& peek() const;
+    bool isEmpty() const;
+    size_t size() const;
 
-    void pop() {
-        if (isEmpty()) {
-            throw std::underflow_error("Stack Underflow: coannot pop from empty stack!");
-        }
-        Node<T>* temp = _top;
-        _top = _top->next;
-        delete temp;
-        --_size;
-    }
+    void clear();
 
-    const T& peek() const {
-        if (isEmpty()) {
-            throw std::runtime_error("Stack is empty: no top element!");
-        }
-        return _top->data;
-    }
-    bool isEmpty() const {
-        return _top == nullptr;
-    }
-
-    size_t size() const {
-        return _size;
-    }
-
-    void clear() {
-        while (!isEmpty()) {
-            pop();
-        }
-    }
-
-    void display() const {
-        Node<T>* temp = _top;
-        while (temp != nullptr) {
-            std::cout << temp->data << " ";
-            temp = temp->next;
-        }
-        std::cout << std::endl;
-    }
+    void display() const;
 
 };
 
--- stack/linkedListStack/linked_List_Stack.tpp
+++ stack/linkedListStack/linked_List_Stack.tpp
@@ -2,4 +2,66 @@
 #ifndef LINKEDLISTSTACK_LINKED_LIST_STACK_TPP
 #define LINKEDLISTSTACK_LINKED_LIST_STACK_TPP
 
+template <typename T>
+Stack<T>::Stack() : _top(nullptr), _size(0) {}
+
+template <typename T>
+Stack<T>::~Stack() {
+    clear();
+}
+template <typename T>
+void Stack<T>::push(const T& value) {
+    Node<T>* newNode = new Node<T>;
+    newNode->data = value;
+    newNode->next = _top;
+    _top = newNode;
+    ++_size;
+}
+
+template <typename T>
+void Stack<T>::pop() {
+    if (isEmpty()) {
+        throw std::underflow_error("Stack Underflow: coannot pop from empty stack!");
+    }
+    Node<T>* temp = _top;
+    _top = _top->next;
+    delete temp;
+    --_size;
+}
+
+template <typename T>
+const T& Stack<T>::peek() const {
+    if (isEmpty()) {
+        throw std::runtime_error("Stack is empty: no top element!");
+    }
+    return _top->data;
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() const {
+    return _top == nullptr;
+}
+
+template <typename T>
+size_t Stack<T>::size() const {
+    return _size;
+}
+
+template <typename T>
+void Stack<T>::clear() {
+    while (!isEmpty()) {
+        pop();
+    }
+}
+
+template <typename T>
+void Stack<T>::display() const {
+    Node<T>* temp = _top;
+    while (temp != nullptr) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+}
+
 #endif
